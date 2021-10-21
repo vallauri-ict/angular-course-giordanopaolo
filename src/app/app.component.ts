70e1e3da3ffc1fab7ab7c53485c864301d6e0e9e
Giordano Paolo <p.giordano.1320@gmail.com>
2021-10-21 11:20:24 +0200

    Added the function add student

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,26 @@ export class AppComponent {
   title = 'Course Giordano Paolo,';
 
   studentList = [
-    { name: "Jolyne Kujo", hobby: "Volley", gender: "F", isPro:false },
-    { name: "Jotaro Kujo", hobby: "Poker", gender: "M", isPro:false },
-    { name: "Joseph Joestar", hobby: "Football", gender: "M", isPro:false },
-    { name: "Foo Fighter", hobby: "Volley", gender: "F", isPro:false },
-    { name: "Bruno Bucciarati", hobby: "Basket", gender: "M", isPro:false },
-    { name: "Giorno Giovanna", hobby: "Volley", gender: "M", isPro:false },
-    { name: "Higashikata Josuke", hobby: "Football", gender: "M", isPro:false },
-    { name: "Jhonatan Joestar", hobby: "Basket", gender: "M", isPro:false }
+    { name: "Jolyne Kujo", hobby: "Wire", gender: "F", isPro: false },
+    { name: "Jotaro Kujo", hobby: "Poker", gender: "M", isPro: true },
+    { name: "Joseph Joestar", hobby: "Football", gender: "M", isPro: false },
+    { name: "Foo Fighter", hobby: "Volley", gender: "F", isPro: false },
+    { name: "Bruno Bucciarati", hobby: "Basket", gender: "M", isPro: false },
+    { name: "Giorno Giovanna", hobby: "Volley", gender: "M", isPro: false },
+    { name: "Higashikata Josuke", hobby: "Football", gender: "M", isPro: false },
+    { name: "Jhonatan Joestar", hobby: "Basket", gender: "M", isPro: false }
   ]
+  @ViewChild("txtName") txtName !: ElementRef;
+  
+  studentGender: string="F";
+  studentName: string = "";
+  studentHobbys:string="";
+
+  hobbies =["Poker", "volley", "dormire", "basket"]
+  onCreateStudent() {
+    let newStudent = { name: this.studentName, hobby: this.studentHobbys, gender: this.studentGender, isPro: false }
+    this.studentList.push(newStudent);
+    this.studentName = "";
+    this.txtName.nativeElement.focus();
+  }
 }
